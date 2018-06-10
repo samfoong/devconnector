3dbb22d19b3a777eec7709129438c36fc20b6c19
Sam <samfoong88@gmail.com>
2018-06-10 20:05:10 +1000

    Creating the JWT for signin token

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:
-    'mongodb://sam:sampassword18@ds161580.mlab.com:61580/devconnector'
+    mongoURI:'mongodb://sam:sampassword18@ds161580.mlab.com:61580/devconnector',
+    secretOrKey: 'secret'
 };
