d213d79997d920576cf5fb462cb266a94976b8f5
Sam <samfoong88@gmail.com>
2018-06-11 19:48:02 +1000

    Validation Handlers II - for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,7 +10,7 @@ const passport = require('passport')
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
-
+const validateLoginInput = require('../../validation/login');
 
 
 // Load User Model
@@ -70,6 +70,13 @@ router.post('/register', (req, res) => {
 // @description     Login user / Returning JWT Token
 // @access          Public
 router.post('/login', (req,res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
     const email = req.body.email;
     const password = req.body.password;
 
@@ -77,7 +84,8 @@ router.post('/login', (req,res) => {
     User.findOne({ email }).then(user => {
             // Check for user
             if(!user) {
-                return res.status(404).json({email: 'User not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             // Check Password
@@ -101,7 +109,8 @@ router.post('/login', (req,res) => {
                         }
                     );
                     } else {
-                    return res.status(400).json({password: 'Password incorrect'});
+                        errors.password = 'Password incorrect';
+                    return res.status(400).json(errors);
                     }
             });
         });
